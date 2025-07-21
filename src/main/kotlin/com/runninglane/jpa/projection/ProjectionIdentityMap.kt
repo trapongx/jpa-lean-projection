338b480ca40a097972a802a66dbd5b26ff8c909a
Teerapong Suksuwan <trapongx@gmail.com>
2025-07-21 21:03:38 +0700

    Rename function parameters in ProjectionIdentityMap to make it clear about the semantics

--- src/main/kotlin/com/runninglane/jpa/projection/ProjectionIdentityMap.kt
+++ src/main/kotlin/com/runninglane/jpa/projection/ProjectionIdentityMap.kt
@@ -6,26 +6,26 @@ internal class ProjectionIdentityMap {
 
     private val map: MutableMap<Triple<KClass<*>, KClass<*>, Any>, Any> = mutableMapOf()
 
-    fun add(entityClass: KClass<*>, projectionClass: KClass<*>, id: Any, projection: Any) {
-        assert(!projectionClass.isAbstract) {
+    fun add(entityClass: KClass<*>, projectionClassImpl: KClass<*>, id: Any, projection: Any) {
+        assert(!projectionClassImpl.isAbstract) {
             "Expected projection class to be concrete"
         }
-        require(projectionClass.java.isAssignableFrom(projection::class.java)) {
+        require(projectionClassImpl.java.isAssignableFrom(projection::class.java)) {
             "Projection class and instance type mismatched"
         }
-        val key = Triple(entityClass, projectionClass, id)
+        val key = Triple(entityClass, projectionClassImpl, id)
         val existing = map[key]
         require(existing == null || projection === existing) {
             "Other projection instance with the same key already exists." +
-                    " entityClass: $entityClass, projection: $projectionClass, id: $id."
+                    " entityClass: $entityClass, projection: $projectionClassImpl, id: $id."
         }
         if (existing == null) map[key] = projection
     }
 
-    fun get(entityClass: KClass<*>, projectionClass: KClass<*>, id: Any): Any? {
-        assert(!projectionClass.isAbstract) {
+    fun get(entityClass: KClass<*>, projectionClassImpl: KClass<*>, id: Any): Any? {
+        assert(!projectionClassImpl.isAbstract) {
             "Expected projection class to be concrete"
         }
-        return map[Triple(entityClass, projectionClass, id)]
+        return map[Triple(entityClass, projectionClassImpl, id)]
     }
 }
