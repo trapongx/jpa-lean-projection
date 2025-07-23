7d38eef199503b5118c8eca6a68e9c09be876bba
Teerapong Suksuwan <trapongx@gmail.com>
2025-07-23 23:30:37 +0700

    Allow @NoProjection to be on property getter

--- src/main/kotlin/com/runninglane/jpa/projection/annotation/NoProjection.kt
+++ src/main/kotlin/com/runninglane/jpa/projection/annotation/NoProjection.kt
@@ -3,6 +3,6 @@ package com.runninglane.jpa.projection.annotation
 import java.lang.annotation.Inherited
 
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.PROPERTY_GETTER)
 @Inherited
 annotation class NoProjection()
