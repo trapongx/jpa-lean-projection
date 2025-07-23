aa449f93ad45e6b5cedb1b5508673674b8e042db
Teerapong Suksuwan <trapongx@gmail.com>
2025-07-23 23:47:52 +0700

    Fix a bug of forgetting to detect annotation on the Java field

--- src/main/kotlin/com/runninglane/jpa/projection/annotation/NoProjectionHelper.kt
+++ src/main/kotlin/com/runninglane/jpa/projection/annotation/NoProjectionHelper.kt
@@ -5,14 +5,20 @@ import javax.persistence.Transient
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.findAnnotations
+import kotlin.reflect.jvm.javaField
 
 internal fun KProperty1<*, *>.isAnnotatedForNoProjection(
     noProjectionAnnotations: Set<KClass<out Annotation>> = emptySet()
 ): Boolean {
     val annotations = noProjectionAnnotations + setOf(NoProjection::class, Transient::class)
+
     return annotations.any { annotation ->
+        // Direct annotations
         findAnnotations(annotation).isNotEmpty()
+                // Getter annotations
                 || getter.findAnnotations(annotation).isNotEmpty()
+                // Java field annotations
+                ||javaField?.isAnnotationPresent(annotation.java) == true
     }
 }
 
