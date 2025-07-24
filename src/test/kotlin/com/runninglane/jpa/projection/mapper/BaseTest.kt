54951cf4ffafce585d053e76dd52a0b069405a3e
Teerapong Suksuwan <trapongx@gmail.com>
2025-07-24 12:58:49 +0700

    Add MapperAssertions for ease of asserting the mapper tree

---
+++ src/test/kotlin/com/runninglane/jpa/projection/mapper/BaseTest.kt
@@ -0,0 +1,58 @@
+package com.runninglane.jpa.projection.mapper
+
+import com.runninglane.jpa.projection.ProjectionFactory
+import com.runninglane.jpa.projection.ProjectorFactory
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import kotlin.reflect.KClass
+
+internal abstract class BaseTest {
+
+    fun createEntityClassMapperWithMocks(
+        entityClass: KClass<*>,
+        projectionClass: KClass<*>,
+        mockProjectionClassImpl: KClass<*>,
+        mockProjectionInstance: Any,
+        mappings: Map<Pair<KClass<*>, KClass<*>>, Pair<KClass<*>, () -> Any>>? = null
+    ): EntityClassMapper = createEntityClassMapperWithMocks(
+        entityClass, projectionClass,
+        mapOf(Pair(
+            (entityClass to projectionClass),
+            (mockProjectionClassImpl to { mockProjectionInstance })
+        )) + (mappings ?: emptyMap())
+    )
+
+    fun createEntityClassMapperWithMocks(
+        entityClass: KClass<*>,
+        projectionClass: KClass<*>,
+        mappings: Map<Pair<KClass<*>, KClass<*>>, Pair<KClass<*>, () -> Any>>
+    ): EntityClassMapper {
+        // Given
+        val projectionFactory = mock(ProjectionFactory::class.java)
+        val projectorFactory = mock(ProjectorFactory::class.java)
+        `when`(projectorFactory.projectionFactory).thenReturn(projectionFactory)
+
+        // Mock the projection class implementation
+        // This works because the methods in ProjectionFactory are marked as 'open'
+        // Without 'open', Mockito can't override the methods and will call the real implementation
+        mappings.forEach { (key, value) ->
+            val (mockProjectionClassImpl, mockProjectionInstance) = value
+            val (entityClass, projectionClass) = key
+
+            `when`(projectionFactory.getImplementation(entityClass, projectionClass))
+                .thenReturn(mockProjectionClassImpl)
+
+            `when`(projectionFactory.create(entityClass, projectionClass))
+                .thenReturn(mockProjectionInstance)
+        }
+
+        return EntityClassMapper.of(
+            projectorFactory,
+            null,
+            entityClass,
+            projectionClass,
+            false,
+            null
+        ) as EntityClassMapper
+    }
+}
