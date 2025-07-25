8bd6993d71e0ef92634d5821ac163b4b02067d13
Teerapong Suksuwan <trapongx@gmail.com>
2025-07-25 16:25:11 +0700

    Fix an error when mapping the same entity type property having value null

--- src/main/kotlin/com/runninglane/jpa/projection/mapper/sametype/SameTypePropertyMapper.kt
+++ src/main/kotlin/com/runninglane/jpa/projection/mapper/sametype/SameTypePropertyMapper.kt
@@ -4,23 +4,18 @@ import com.runninglane.jpa.projection.mapper.Mapper
 import com.runninglane.jpa.projection.mapper.PropertyAccessor
 import com.runninglane.jpa.projection.mapper.PropertyMapper
 import com.runninglane.jpa.projection.reflection.annotatedWith
-import javax.persistence.EmbeddedId
-import javax.persistence.Id
-import javax.persistence.ManyToOne
-import javax.persistence.OneToOne
+import javax.persistence.*
 import javax.persistence.criteria.Expression
 import javax.persistence.criteria.From
 import javax.persistence.criteria.JoinType
 import javax.persistence.criteria.Path
-import javax.persistence.criteria.Root
 import kotlin.reflect.KClass
 import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.jvmErasure
 
 internal class SameTypePropertyMapper(
-    parent: Mapper?,
-    entityClass: KClass<*>,
-    projectionClassImpl: KClass<*>,
+    private val parent: Mapper?,
+    private val entityClass: KClass<*>,
+    private val projectionClassImpl: KClass<*>,
     override val propertyName: String
 ) : SameTypeMapper(parent), PropertyMapper {
 
@@ -33,13 +28,17 @@ internal class SameTypePropertyMapper(
 
     val isIdProperty: Boolean = prop.let { it.annotatedWith<Id>() || it.annotatedWith<EmbeddedId>() }
 
-    private val isAssociation = prop.let { it.annotatedWith<OneToOne>() || it.annotatedWith<ManyToOne>() }
+    private val isAssociationOrEmbedded = prop.let {
+        it.annotatedWith<OneToOne>() || it.annotatedWith<ManyToOne>()
+                || it.annotatedWith<Embedded>() || it.annotatedWith<EmbeddedId>()
+    }
 
     private val propertyAccessor = PropertyAccessor.of(entityClass, projectionClassImpl, propertyName)
 
     override fun buildValueExpression(path: Path<*>): Expression<*> {
         return when {
-            isAssociation -> (path as From<*, *>).join<Any, Any>(propertyName, JoinType.LEFT)
+            isAssociationOrEmbedded -> (path as From<*, *>).join<Any, Any>(propertyName, JoinType.LEFT)
+
             else -> path.get<Any>(propertyName)
         }
     }
